f6c966408dca6581d36bc7672fe6c3ca3d416c81
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-06-04 20:29:58 +0200

    Adat tagok és alap konstruktor

--- src/modell/KincsesLada.java
+++ src/modell/KincsesLada.java
@@ -1,14 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 
 package modell;
 
-/**
- * 
- * @author Sammy Guergachi <sguergachi at gmail.com>
- */
-public class KincsesLada {
 
+public class KincsesLada {
+    private final String[] ALLITASOK = {"én rejtem a kincset", "nem én rejtem a kincset", "az arany hazudik"};
+    private String szin;
+    private String allitas;
+    private Boolean kincses_e;
+
+    public KincsesLada(String szin, String allitas, Boolean kincses_e) {
+        setSzin(szin);
+        setAllitas(szin);
+        setKincses_e(kincses_e);
+    }
+    
+    
+    
+
+    private void setSzin(String szin) {
+        this.szin = szin;
+    }
+
+    private void setAllitas(String szin) {
+        if(szin.equals("arany")){
+            this.allitas = ALLITASOK[0];
+        }else if(szin.equals("ezüst")){
+            this.allitas = ALLITASOK[1];
+        }else if(szin.equals("bronz")){
+            this.allitas = ALLITASOK[2];
+        }
+    }
+
+    private void setKincses_e(Boolean kincses_e) {
+        this.kincses_e = kincses_e;
+    }
+    
+    
 }
+
+
