44358e096b186a7e781dc4b8e710680c6da038fb
konnan933 <95681490+konnan933@users.noreply.github.com>
2022-06-04 21:31:23 +0200

    Első tesztesetek megírása adattagokra

--- src/main/Program.java
+++ src/main/Program.java
@@ -6,12 +6,6 @@ public class Program {
 
     
     public static void main(String[] args) {
-        KincsesLada arany = new KincsesLada("Arany");
-        System.out.println(arany.toString());
-        KincsesLada ezust = new KincsesLada("Ezüst");
-        System.out.println(ezust.toString());
-        KincsesLada bronz = new KincsesLada("Bronz");
-        System.out.println(bronz.toString());
         
     }
     
--- src/teszt/Teszt.java
+++ src/teszt/Teszt.java
@@ -1,6 +1,40 @@
-
 package teszt;
+import modell.KincsesLada;
 
 public class Teszt {
+   
+    public static void main(String[] args) {
+        new Teszt();  
+    }
 
+    public Teszt() {
+        szinTeszt();
+        kincses_eTeszt();
+        allitasTeszt();
+        
+    }
+   
+   
+   public void szinTeszt (){
+       KincsesLada arany = new KincsesLada("arany");
+       if(arany.getSzin().equals("arany")){
+           System.out.println("Névteszt végigment sikeresen!");
+       }
+  }
+   
+   public void kincses_eTeszt(){
+       KincsesLada ezust = new KincsesLada("ezüst");
+       if (ezust.getKincses_e() == true) {
+           System.out.println("Kincses_e teszt végigment sikeresen");
+       }
+   }
+   public void allitasTeszt(){
+       KincsesLada bronz = new KincsesLada("bronz");
+       if(bronz.getAllitas()=="az arany hazudik"){
+           System.out.println("Az állitás teszt végigment sikeresen");
+       }
+   }
+    
+    
+    
 }
